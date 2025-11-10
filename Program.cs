85e8e440aed2502c168b3e486ab50d6a8aff19f8
tomaszsikora22578-png <tomasz.sikora.22578@gmail.com>
2025-11-10 11:28:01 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using ArWidgetApi.Models;
 using ArWidgetApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
-using Pomelo.EntityFrameworkCore.MySql.Storage.Internal; // Dla MySqlUnixDomainSocketFactory
 using ArWidgetApi.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,76 +9,87 @@ var builder = WebApplication.CreateBuilder(args);
 // Nazwa polityki CORS
 const string ClientAppCORS = "_clientAppCORS";
 
-// 1. Serwisy
+// -------------------- 1. Serwisy --------------------
+
+// Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// 2. CORS
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(ClientAppCORS, policy =>
     {
         policy.WithOrigins(
-                "http://127.0.0.1:5500",
-                "https://tomaszsikora22578-png.github.io",
-                "https://ar-widget-project.firebaseapp.com",
-                "https://ar-widget-project.web.app"
-            )
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+                    "http://127.0.0.1:5500",
+                    "https://tomaszsikora22578-png.github.io",
+                    "https://ar-widget-project.firebaseapp.com",
+                    "https://ar-widget-project.web.app"
+                )
+                .AllowAnyHeader()
+                .AllowAnyMethod();
     });
 });
 
-// 3. Baza danych (MySQL)
+// -------------------- 2. Konfiguracja Bazy Danych --------------------
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// Cloud Run automatycznie ustawia tę zmienną środowiskową, jeśli dodałeś połączenie do Cloud SQL
-var cloudSqlInstance = builder.Configuration["CLOUD_SQL_CONNECTION_NAME"]; 
+// Cloud Run + Cloud SQL
+var cloudSqlInstance = builder.Configuration["CLOUD_SQL_CONNECTION_NAME"];
 var isCloudRun = !string.IsNullOrEmpty(cloudSqlInstance);
 
-// Użycie Pomelo + MySQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
-    var serverVersion = ServerVersion.Create(8, 0, 34, ServerType.MySql);
+    // AutoDetect wykrywa MySQL lub MariaDB na podstawie connection string
+    var serverVersion = ServerVersion.AutoDetect(connectionString);
 
     if (isCloudRun)
     {
-        // Połączenie przez UNIX socket (Cloud SQL)
-        options.UseMySql(connectionString, serverVersion, mysqlOptions =>
+        // Połączenie przez UNIX socket (Cloud SQL w Cloud Run)
+        options.UseMySql(connectionString, serverVersion, mySqlOptions =>
         {
-            mysqlOptions
-                .UseMySqlOptions(conn => conn
-                    .SocketFactory(typeof(MySqlUnixDomainSocketFactory))
-                );
+            mySqlOptions.UseUnixSocket($"/cloudsql/{cloudSqlInstance}");
+            mySqlOptions.EnableRetryOnFailure();
         });
-
-        Console.WriteLine($"[INFO] Użyto połączenia Cloud SQL przez UNIX socket: {cloudSqlInstance}");
+        Console.WriteLine($"[INFO] Użyto połączenia UNIX socket dla Cloud SQL: {cloudSqlInstance}");
     }
     else
     {
-        // Standardowe połączenie TCP (lokalne środowisko dev)
+        // Lokalny MySQL
         options.UseMySql(connectionString, serverVersion);
         Console.WriteLine("[INFO] Użyto lokalnego połączenia MySQL.");
     }
 });
 
-// 4. Kontrolery i autoryzacja
+// -------------------- 3. Inne serwisy --------------------
 builder.Services.AddControllers();
 builder.Services.AddAuthorization();
 
-// --- 5. Budowanie aplikacji ---
+// -------------------- 4. Budowanie aplikacji --------------------
 var app = builder.Build();
 
+// Swagger w Development
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// HTTPS redirect
 app.UseHttpsRedirection();
+
+// CORS
 app.UseCors(ClientAppCORS);
+
+// Autoryzacja
 app.UseAuthorization();
+
+// Middleware weryfikujący token klienta
 app.UseMiddleware<ClientTokenMiddleware>();
+
+// Mapowanie endpointów
 app.MapControllers();
 
+// Uruchomienie
 app.Run();
