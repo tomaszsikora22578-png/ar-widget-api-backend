b89e16e327a2aea15324b3206564e3f91506f743
tomaszsikora22578-png <tomasz.sikora.22578@gmail.com>
2025-11-14 08:21:01 +0100

    Create ModelDataDto.cs

---
+++ Models/ModelDataDto.cs
@@ -0,0 +1,14 @@
+namespace ArWidgetApi.Models
+{
+    public class ModelDataDto
+    {
+        // Pola mapowane z bazy danych
+        public string Name { get; set; } 
+        public string ModelUrlGlb { get; set; } 
+        public string ModelUrlUsdz { get; set; }
+        
+        // Pola na generowane Signed URL (zwracane do frontendu)
+        public string SignedUrlGlb { get; set; }
+        public string SignedUrlUsdz { get; set; } 
+    }
+}
