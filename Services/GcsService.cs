696cabd71182a355fd11a9f33dc27e6490eac6d0
tomaszsikora22578-png <tomasz.sikora.22578@gmail.com>
2025-11-14 10:27:21 +0100

    Update GcsService.cs

--- Services/GcsService.cs
+++ Services/GcsService.cs
@@ -2,33 +2,33 @@
 using Google.Cloud.Storage.V1; 
 using System.Net.Http;
 using System;
-// USUŃ: using Google.Apis.Auth.OAuth2; 
-// USUŃ: using Google.Cloud.Storage.V1.Signing; // Tego i tak nie widać
+using Google.Apis.Auth.OAuth2; // To jest potrzebne dla GoogleCredential
+using Google.Cloud.Storage.V1.Signing; // Ta linijka jest KLUCZOWA (i problematyczna)
 
 namespace ArWidgetApi.Services 
 {
     public class GcsService
     {
-        private readonly StorageClient _storageClient; // Teraz używamy instancji klienta
+        private readonly UrlSigner _urlSigner; // Zmieniamy z powrotem na UrlSigner
         private const string BucketName = "ar-models-dla-klientow"; 
-        
-        // Ta metoda nie działa w Cloud Run bez klucza JSON:
-        // private readonly GoogleCredential _credential; 
 
         public GcsService()
         {
-            // StorageClient.Create() automatycznie pobiera poświadczenia konta serwisowego
-            // Cloud Run, co jest KLUCZOWE dla działania tej metody.
-            _storageClient = StorageClient.Create(); 
+            // GoogleCredential.GetApplicationDefault() jest asynchroniczne,
+            // ale działa synchronicznie w Cloud Run do pobrania poświadczeń.
+            var credential = GoogleCredential.GetApplicationDefault();
+            
+            // To jest instancja, która prawidłowo obsługuje podpisywanie w Cloud Run
+            _urlSigner = UrlSigner.FromCredential(credential); 
         }
 
         public string GenerateSignedUrl(string objectName)
         {
-            var expiration = DateTime.UtcNow.AddMinutes(5); 
+            var expiration = DateTimeOffset.UtcNow.AddMinutes(5); 
 
-            // Używamy metody instancyjnej StorageClient
-            string signedUrl = _storageClient.CreateSignedUrl(
-                bucket: BucketName,
+            // Używamy metody instancyjnej Sign z UrlSigner
+            string signedUrl = _urlSigner.Sign(
+                bucketName: BucketName,
                 objectName: objectName,
                 expiration: expiration,
                 method: HttpMethod.Get
