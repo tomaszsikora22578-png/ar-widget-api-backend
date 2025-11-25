da34cb6fea3bd13a2404e9df00b621a6f685625c
tomaszsikora22578-png <tomasz.sikora.22578@gmail.com>
2025-11-25 10:13:34 +0100

    Update AnalyticsEntry.cs

--- Models/AnalyticsEntry.cs
+++ Models/AnalyticsEntry.cs
@@ -2,11 +2,12 @@ namespace ArWidgetApi.Models
 {
     public class AnalyticsEntry
     {
-        public int Id { get; set; }
-        public int ClientId { get; set; } // FK
-        public Client Client { get; set; }
-        public string ProductId { get; set; } // Ktory produkt zostal klikniety
-        public string EventType { get; set; } = "AR_CLICK";
-        public DateTime Timestamp { get; set; }
+       public int Id { get; set; }
+        public int ClientId { get; set; } // FK
+        // ✅ POPRAWKA CS8618: Inicjalizacja domyślna dla relacji
+        public Client Client { get; set; } = default!; 
+        public int ProductId { get; set; } 
+        public string EventType { get; set; } = "AR_CLICK";
+        public DateTime Timestamp { get; set; }
     }
 }
