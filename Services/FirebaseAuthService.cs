f0f5dcd67fc2ac6255aea9c0513f4fb7dfa5a4f3
tomaszsikora22578-png <tomasz.sikora.22578@gmail.com>
2025-12-08 13:14:28 +0100

    Create FirebaseAuthService.cs

---
+++ Services/FirebaseAuthService.cs
@@ -0,0 +1,23 @@
+using FirebaseAdmin.Auth;
+
+public interface IFirebaseAuthService
+{
+    Task<FirebaseToken> VerifyIdTokenAsync(string idToken);
+}
+
+public class FirebaseAuthService : IFirebaseAuthService
+{
+    public async Task<FirebaseToken> VerifyIdTokenAsync(string idToken)
+    {
+        try
+        {
+            var decodedToken = await FirebaseAuth.DefaultInstance.VerifyIdTokenAsync(idToken);
+            return decodedToken;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[WARN] Nieprawidłowy token: {ex.Message}");
+            return null;
+        }
+    }
+}
